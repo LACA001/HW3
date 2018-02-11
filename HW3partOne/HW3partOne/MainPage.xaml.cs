03a5f0a8194724379159ebc9890df103cdb105bc
laca001 <llaca7601@gmail.com>
2018-02-10 21:49:44 -0800

    Add project files.

---
+++ HW3partOne/HW3partOne/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HW3partOne
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+		}
+
+        void Plan1Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Plan1());
+        }
+
+        void Plan2Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Plan2());
+        }
+
+        void Plan3Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Plan3());
+        }
+    }
+}
---
+++ HW3partOne/HW3partOne/Plan3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HW3partOne
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Plan3 : ContentPage
+	{
+		public Plan3()
+		{
+			InitializeComponent ();
+		}
+	}
+}
